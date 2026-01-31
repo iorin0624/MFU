85cacee897a2f56f0cfd276ad1eec32c235a243f
iorin0624 <mmz@mail.iori0624.jp>
2026-01-31 18:46:03 +0900

    sync from 103.16 (/mnt/mfu/app)

--- migrations/202602_receipts_payer_info.sql
+++
@@ -1,7 +0,0 @@
--- 受領書機能: 支払者情報の追加
-
-ALTER TABLE receipts
-  ADD COLUMN payer_name VARCHAR(255) NOT NULL AFTER issuer_user_id,
-  ADD COLUMN payer_address VARCHAR(255) NOT NULL AFTER payer_name,
-  ADD COLUMN payer_phone VARCHAR(64) NOT NULL AFTER payer_address,
-  ADD COLUMN payer_email VARCHAR(255) NOT NULL AFTER payer_phone;
--- migrations/202611_user_passkeys_label.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE user_passkeys
-  ADD COLUMN label VARCHAR(128) NULL AFTER username;
