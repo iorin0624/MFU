6d162898fa347f7edf240784ef88182d03c1e7d9
iorin0624 <mmz@mail.iori0624.jp>
2026-03-25 16:35:06 +0900

    sync from 103.16 (/mnt/mfu/app)

--- __init__.py
+++ __init__.py
@@ -129,7 +129,7 @@ app = Flask(
     static_folder=STATIC_DIR,
     static_url_path="/static",
 )
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
 
 @app.get("/manifest.webmanifest")
